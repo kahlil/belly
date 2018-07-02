c88ca456ca60ec770780d641552714b7dab2c80b
Kahlil Lechelt <kahlil.lechelt@logmein.com>
2018-07-02 15:14:04 +0200

    remove tests for now

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import test from 'ava';
-import execa from 'execa';
+// import test from 'ava';
+// import execa from 'execa';
 
-test('title', async t => {
-	t.is(await execa.stdout('./cli.js', ['ponies']), 'ponies & rainbows');
-});
+// test('title', async t => {
+// 	t.is(await execa.stdout('./cli.js', ['ponies']), 'ponies & rainbows');
+// });
